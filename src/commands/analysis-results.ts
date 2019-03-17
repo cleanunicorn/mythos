c04d4bbefda76ee88960b5540baca0dc17ef1c32
tagomaru <tagomaru@users.noreply.github.com>
2019-03-17 20:25:36 +0900

    Add analysis-results command

---
+++ src/commands/analysis-results.ts
@@ -0,0 +1,70 @@
+import {Command, flags} from '@oclif/command'
+import * as armlet from 'armlet'
+import cli from 'cli-ux'
+import * as fs from 'fs'
+
+import {Sourcemap} from '../sourcemap'
+
+export default class AnalysisResults extends Command {
+  static description = 'Retrieve analysis results scanned with MythX API'
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    mythxEthAddress: flags.string({
+      env: 'MYTHX_ETH_ADDRESS',
+      required: true,
+      hidden: false,
+    }),
+    mythxPassword: flags.string({
+      env: 'MYTHX_PASSWORD',
+      required: true,
+    }),
+  }
+
+  static args = [
+    {
+      name: 'uuid',
+      required: true,
+      description: 'uuid to retrive analysis results',
+    },
+  ]
+
+  async run() {
+    // Parse args and flags
+    const {args, flags} = this.parse(AnalysisResults)
+    const mythxAddress = flags.mythxEthAddress
+    const mythxPassword = flags.mythxPassword
+
+    const uuid = args.uuid
+
+    // Get armlet instance
+    const client = new armlet.Client({
+      password: mythxPassword,
+      ethAddress: mythxAddress,
+    })
+
+    // Retrieve analysis results with UUID
+    let results = {issues : []}
+    cli.action.start(`Retrieving analysis results: ${uuid}`)
+    try {
+      results.issues = await client.getIssues(uuid)
+    } catch (error) {
+      this.error(error)
+      cli.action.stop('failed')
+      return
+    }
+    cli.action.stop('done')
+
+    // Write report
+    fs.writeFileSync(`./issues_${uuid}.json`, JSON.stringify(results, null, 4), 'utf-8')
+
+    // Display report
+    this.log(`Report found ${results.issues[0].issues.length} issues`)
+    const contractFile = results.issues[0].sourceList[0]
+    const contractFileContent = fs.readFileSync(contractFile, {encoding: 'utf-8'})
+    const s = new Sourcemap(contractFile, contractFileContent, results)
+    this.log(s.output().join('\n'))
+
+    this.log('Done')
+  }
+}
